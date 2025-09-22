e537b79d4a52708d4473f50e23c3cbb70aaa43d9
bequshabeqa <bekatchitadze@gmail.com>
2025-09-22 19:47:09 +0400

    davaleba 13

---
+++ 13/main.js
@@ -0,0 +1,10 @@
+let color = "blue";
+let text = "გამარჯობა";
+
+let box = document.getElementById("box");
+box.textContent = text;
+
+box.style.backgroundColor = color;
+box.style.color = "white";
+box.style.width = "200px";
+box.style.height = "200px";
