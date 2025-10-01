1c820be103ec39d12da26d0f8289150cb084288d
bequshabeqa <bekatchitadze@gmail.com>
2025-10-01 23:53:13 +0400

    final update

---
+++ Final/main.js
@@ -0,0 +1,10 @@
+let Contact = document.getElementById("Contact");
+
+
+Contact.addEventListener("mouseover", () => {
+    Contact.style.color = "red";
+});
+
+Contact.addEventListener("mouseout", () => {
+    Contact.style.color = "white";
+});
